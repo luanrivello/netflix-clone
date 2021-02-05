a4821653897a1744ca84c1e450a51ce2d0867809
Luan Belem <luanrivello@hotmail.com>
2021-02-05 00:58:50 -0300

    first commit

---
+++ script/owl/setup.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:20,
+    nav:false,
+    responsive:{
+        0:{
+            items:3
+        },
+        1200:{
+            items:6
+        },
+        1600:{
+            items:8
+        }
+    }
+})
